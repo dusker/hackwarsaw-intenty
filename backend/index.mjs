3319c3b23eb87e1e73467ed8d9e0a1666d70b43b
Michal Tuszynski <srgtuszy@gmail.com>
2024-06-15 22:30:29 +0200

    Added a scrolling list with the products

--- backend/index.mjs
+++ backend/index.mjs
@@ -45,7 +45,7 @@ const transcribeAudio = async (filePath) => {
 app.post('/upload-audio', upload.single('audio'), async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   if (!req.file) {
     const error = {message: 'No audio file uploaded'};
     sendError(res, error);
